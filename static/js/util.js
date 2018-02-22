f5aa61ec5145f78d6ed1cc4605322767be3c3c68
aftermoon <callmeaftermoon@163.com>
2018-02-22 13:38:23 +0800

    test

--- static/js/util.js
+++ static/js/util.js
@@ -1,21 +1,23 @@
-var url = '//oss.chengchangqin.com/jxyhs/%E6%A1%A5%E6%A2%81%E5%9F%BA%E6%9C%AC%E7%8A%B6%E5%86%B5%E5%8D%A1%E7%89%87.pdf'
-PDFJS.workerSrc = '/static/js/pdf.worker.min.js'
-var loadingTask = PDFJS.getDocument(url);
-loadingTask.promise.then(function(pdf) {
-  for (var i = 1; i <= pdf.numPages; i++) {
-    pdf.getPage(i).then(function(page) {
-      var id = "canvas"+i;
-      var scale = 1.2;
-      var viewport = page.getViewport(scale);
-      var canvas = document.getElementById(id);
-      var context = canvas.getContext('2d');
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      var renderContext = {
-        canvasContext: context,
-        viewport: viewport
-      };
-      page.render(renderContext);
-    });
-  };
+$(document).ready(function() {
+  var url = '//oss.chengchangqin.com/jxyhs/%E6%A1%A5%E6%A2%81%E5%9F%BA%E6%9C%AC%E7%8A%B6%E5%86%B5%E5%8D%A1%E7%89%87.pdf'
+  PDFJS.workerSrc = '/static/js/pdf.worker.min.js'
+  var loadingTask = PDFJS.getDocument(url);
+  loadingTask.promise.then(function(pdf) {
+    for (var i = 1; i <= pdf.numPages; i++) {
+      pdf.getPage(i).then(function(page) {
+        var id = "canvas"+i;
+        var scale = 1.2;
+        var viewport = page.getViewport(scale);
+        var canvas = document.getElementById(id);
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        page.render(renderContext);
+      });
+    };
+  });
 });
